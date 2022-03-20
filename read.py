9fd3b5d480d18ec35610fa7928f65a4a62dbc248
zekeby <51679893+zekeby@users.noreply.github.com>
2022-03-20 10:56:19 +0300

    Add files via upload

---
+++ read.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+while True: 
+    f1 = open('/var/log/auth.log', 'r+')
+    data1 = f1.read()
+#    print(data1)
+    f1.close()
+    time.sleep(1)
+    f2 = open('/var/log/auth.log', 'r+')
+    data2 = f2.read()
+#    print(data2)
+    f2.close()
+
+    if data1 != data2:
+        os.system("tail -n 1 /var/log/auth.log | grep ssh")
